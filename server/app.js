96e9c8f8cf728ce7eefd2cdbd42e78462773fe23
amith-342 <90054229+Amith-342@users.noreply.github.com>
2023-03-17 00:54:06 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 //     }))
 app.use(express.json())
 
-const port = process.env.port||5000;
+const port = process.env.PORT||5000;
 
 const Product = require("../models/products")
 
